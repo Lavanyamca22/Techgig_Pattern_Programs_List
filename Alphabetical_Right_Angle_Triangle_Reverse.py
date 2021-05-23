84f9e4329e33fbc23452ceddd41baeeabb0a9931
Lavanyamca22 <70351093+Lavanyamca22@users.noreply.github.com>
2021-05-23 13:31:31 +0530

    Create Alphabetical_Right_Angle_Triangle_Reverse.py
    You need to take an integer input and then draw the pattern according to it. Say for example if you enter 5 then, the pattern should be like this-
    A A A A A
    B B B B
    C C C
    D D
    E

---
+++ Alphabetical_Right_Angle_Triangle_Reverse.py
@@ -0,0 +1,8 @@
+import string
+def main(n):
+    # Write code here 
+    s = string.ascii_uppercase[0:n]
+    for i in range(0,n):
+        print(*[s[i] for j in range(n,i,-1)])
+n = int(input())
+main(n)
