14bd463d2cde6b1708e0a69f8a0cc94796b17ba4
Lavanyamca22 <70351093+Lavanyamca22@users.noreply.github.com>
2021-05-23 15:25:00 +0530

    Create Ascending_Alphabetical_Left_Angle_Triangle_Reverse.py
    You need to take an integer input and then draw the pattern according to it. Say for example if you enter 5 then, the pattern should be like this-
    
    A B C D E
       A B C D
          A B C
             A B
                A

---
+++ Ascending_Alphabetical_Left_Angle_Triangle_Reverse.py
@@ -0,0 +1,12 @@
+import string
+def main(n):
+    # Write code here 
+    s = string.ascii_uppercase[0:n]
+    count = 0
+    for i in range(n,-1,-1):
+        print('  '*count,end='')
+        print(*[s[j] for j in range(0,i)])
+        count = count+1 
+
+n = int(input())
+main(n)
